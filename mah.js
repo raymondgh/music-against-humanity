1c66d7fa6e7ed158c069574f8bac67e1aa4fcd6d
sasilukr <p.pandapower@gmail.com>
2014-05-18 11:28:15 -0700

    Deploy and all JS lib

--- mah.js
+++ mah.js
@@ -18,15 +18,19 @@
 		var players = {};
 		players["ray"] = {
 			"status" : "inactive",
+			"score" : 0,
 			"image": "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash2/t1.0-1/c1.0.148.148/p148x148/10153124_10152034868947124_4493190993143905922_n.jpg"};
 		players["sasi"] = {
 			"status" : "inactive",
+			"score" : 0,
 			"image": "https://scontent-b-sjc.xx.fbcdn.net/hprofile-ash2/l/t1.0-1/c31.0.148.148/p148x148/10007032_10152266803177937_365546827_n.jpg"};
 		players["tan"] = {
 			"status" : "inactive",
+			"score" : 0,
 			"image": "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/t1.0-1/c41.30.372.372/s148x148/76725_1665291162374_4704204_n.jpg"};
 		players["michel"] = {
 			"status" : "inactive",
+			"score" : 0,
 			"image": "https://scontent-a-sjc.xx.fbcdn.net/hphotos-frc3/t1.0-9/10372123_10152405557367937_7944549406127823943_n.jpg"};
 		var playersRef = new Firebase('https://mah.firebaseio.com/players');
 		playersRef.set(players);
@@ -133,6 +137,8 @@
 	function submitAnswer(player, answer) {
 		var statusRef = new Firebase('https://mah.firebaseio.com/status');
 		var playingRef = new Firebase('https://mah.firebaseio.com/playing');
+		var scoreRef = new Firebase('https://mah.firebaseio.com/players/'+player+'/score');
+
 
 		statusRef.once('value', function(sSnapshot) {
 			var status = sSnapshot.val();
@@ -140,12 +146,41 @@
 				playingRef.once('value', function(pSnapshot) {
 					var playing = pSnapshot.val();
 					var roundsRef = new Firebase('https://mah.firebaseio.com/rounds/'+playing+'/guesses/'+player);
-				
+					var winRef = new Firebase('https://mah.firebaseio.com/rounds/'+playing+'/winners');
 					roundsRef.set(answer) ;
+
+					scoreRef.once('value', function(scoreSnapshot) {
+						var score = scoreSnapshot.val();
+						if ( getOwner() == answer ) {
+							scoreRef.set(score++);
+							winRef.once('value', function(wSnapshot) {
+								var winners = winRef.val();
+								if ( winners === null ) {
+									winners = [];
+								}
+								winners.push(player);
+								winRef.set(winners);
+							});
+						}
+					});
+					
 				});
 			}
 		});
 	}
+
+	function getOwner() {
+		var playingRef = new Firebase('https://mah.firebaseio.com/playing');
+		playingRef.once('value', function(pSnapshot) {
+			var playing = pSnapshot;
+			var playlist = getPlaylist();
+			var owner = playlist[playing]['owner'];
+			console.log("Playlist Track " + owner);
+			return owner; 
+		});
+	}
+
+
 
 	function getPlaylist() {
 		BeatsService.fetchAllPlaylist(function (data) { globalPlaylist = data; });
