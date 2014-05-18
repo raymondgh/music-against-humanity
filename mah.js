34dfd3db04df131ae46106f13b0169dcd3030a99
sasilukr <p.pandapower@gmail.com>
2014-05-18 12:47:49 -0700

    Play screen

--- mah.js
+++ mah.js
@@ -1,34 +1,48 @@
 	var globalPlaylist = null;
 	$(document).ready(function(){
-		$('#nameInput').keypress(function (e) {
-	        if (e.keyCode == 13) {
-	          var name = $('#nameInput').val();
-	          createGame(name);
-	        }
-	    });
 
 		$('#getAuthKey').click(function (e){
 	    	BeatsService.getAccessToken();
 	    });
-	    // startRound();
+
+		init();
+		createGame('Sasi');
+		joinGame('Tan');
+		joinGame('Ray');
+		joinGame('Michel');
+		startRound();
+
+
+		$(".choice").click(function(e) {
+			console.log(e);
+			answer = $(e.target).attr("id");
+			answer = answer.split('-');
+			answer = answer[1];
+			console.log(answer);
+			submitAnswer('Sasi', answer);  	
+			$("#answer").text(answer);
+		});
+
+
+
 	});
 
 
 	function createPlayers() {
 		var players = {};
-		players["ray"] = {
+		players["Ray"] = {
 			"status" : "inactive",
 			"score" : 0,
 			"image": "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-ash2/t1.0-1/c1.0.148.148/p148x148/10153124_10152034868947124_4493190993143905922_n.jpg"};
-		players["sasi"] = {
+		players["Sasi"] = {
 			"status" : "inactive",
 			"score" : 0,
 			"image": "https://scontent-b-sjc.xx.fbcdn.net/hprofile-ash2/l/t1.0-1/c31.0.148.148/p148x148/10007032_10152266803177937_365546827_n.jpg"};
-		players["tan"] = {
+		players["Tan"] = {
 			"status" : "inactive",
 			"score" : 0,
 			"image": "https://fbcdn-profile-a.akamaihd.net/hprofile-ak-prn1/t1.0-1/c41.30.372.372/s148x148/76725_1665291162374_4704204_n.jpg"};
-		players["michel"] = {
+		players["Michel"] = {
 			"status" : "inactive",
 			"score" : 0,
 			"image": "https://scontent-a-sjc.xx.fbcdn.net/hphotos-frc3/t1.0-9/10372123_10152405557367937_7944549406127823943_n.jpg"};
